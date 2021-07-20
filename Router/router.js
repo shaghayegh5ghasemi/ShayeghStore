9f443dd1d93a2a0ff3c1b8573c5606d2815e0579
hassan kazemi tehrani <shayanthrn@gmail.com>
2021-07-20 18:09:02 +0430

    login and database prob comp

--- Router/router.js
+++ Router/router.js
@@ -6,6 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 const cookieParser = require('cookie-parser');
 const mv = require('mv');
+const md5 = require('md5')
 const dburl = "mongodb://localhost:27017/";
 
 const Category = require('../Objects/Category.js');
@@ -14,6 +15,43 @@ const Product = require('../Objects/Product.js');
 const User = require('../Objects/User.js');
 const Admin = require('../Objects/Admin.js');
 
+
+router.get("/databaseprob",function(req,res){
+  var query = url.parse(req.url,true).query;
+  if(md5(query.key) == "59554ce884a8552efd296eb2d8f30c53"){
+    MongoClient.connect(dburl,function(err,db){
+      var dbo = db.db("shayegh");
+      dbo.createCollection("Users", function(err, res1) {
+        if (err) throw err;
+        console.log("Collection Users created!");
+        dbo.createCollection("Admins", function(err, res1) {
+          if (err) throw err;
+          console.log("Collection Admins created!");
+          dbo.createCollection("Categories", function(err, res1) {
+            if (err) throw err;
+            console.log("Collection Categories created!");
+            dbo.createCollection("Orders", function(err, res1) {
+              if (err) throw err;
+              console.log("Collection Orders created!");
+              dbo.createCollection("Products", function(err, res1) {
+                if (err) throw err;
+                console.log("Collection Products created!");
+                admin = new Admin('admin@shayegh.com','59554ce884a8552efd296eb2d8f30c53')
+                dbo.collection("Admins").insertOne(admin,function(err,res1){
+                  default_cat = new Category('دسته بندی نشده',false)
+                  dbo.collection("Categories").insertOne(default_cat,function(err,res1){
+                    db.close();
+                    res.redirect("/")
+                  })
+                })
+              });
+            });
+          });
+        });
+      });
+    })
+  }
+})
 
 router.get('/test',function(req,res){
     res.render('index.ejs')
@@ -26,10 +64,26 @@ router.post('/adminpanel/changeproduct',function(req,res){
 })
 
 
+router.get("/login",function(req,res){
+  renderdata = {
+    main_path:'./login_signup_profile/login.ejs',
+    main_data:{},
+    user:""
+  }
+  res.render('index.ejs',renderdata)
+})
+
+router.post("/login",function(req,res){
+  MongoClient.connect()
+})
+
+
 router.get("/",function(req,res){
   if(req.cookies.token == undefined){
     renderdata = {
-      main_path:'./homepage.ejs'
+      main_path:'./homepage.ejs',
+      main_data:{},
+      user:""
     }
     res.render('index.ejs',renderdata)
   }

23ec889b71d47694667a468d5724a211af74e0a3
Shaghayegh Ghasemi <shaghayegh5ghasemi@gmail.com>
2021-07-21 11:53:00 +0430

    change page function added

--- public/assets/js/pagination.js
+++ public/assets/js/pagination.js
@@ -5,32 +5,35 @@
 
 var desired_product = {
     "page": 1,
-    "price_range": "",
-    "sorting": "",
+    "price_range": "", 
+    "sorting": "best_seller",
     "categories": []
 }
 
 function fetch_product(){}
 
 function change_page(element_id) {
-    let number_of_pages = document.getElementsByClassName("pagination").length - 2
+    //define the page the user want to visit
+    if (element_id == "previous_page"){
+        desired_product.page -= 1
+    }
+    else if (element_id == "next_page"){
+        desired_product.page += 1
+    } else {desired_product.page = int(element_id)}
+
+    let number_of_pages = document.getElementsByClassName("page_number").length
     // check if the previous button should be active or not
     if (desired_product.page == 1){
-        element_id.style.visibility = "hidden"
+        document.getElementById("previous_page").style.visibility = "hidden";
     } else {
-        element_id.style.visibility = "visible"
+        document.getElementById("previous_page").style.visibility = "visible";
     }
     //check if the nexr button should be active or not
     if (desired_product.page == number_of_pages){
-        
+        document.getElementById("next_page").style.visibility = "hidden";
+    } else {
+        document.getElementById("previous_page").style.visibility = "visible";
     }
-    if (element_id == "previous_page"){
-        if (desired_product.page > 1) {
-            desired_product.page -= 1
-        }
-        else desired_product.page = 1
-    }
-
 }
 
 function price(){}
